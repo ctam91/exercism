5a82a541affeb61b234f145503373bf0e6b98001
ctam91 <ctam91@gmail.com>
2017-10-20 19:14:02 -0700

    complete challenge

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,26 @@
 class RnaTranscription {
 
-    String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    String transcribe(String dnaStrand) throws IllegalArgumentException {
+        StringBuilder rnaStrand = new StringBuilder();
+        char[] dna = dnaStrand.toCharArray();
+        for (char character : dna) {
+            switch (character) {
+                case 'G':
+                    rnaStrand.append('C');
+                    break;
+                case 'C':
+                    rnaStrand.append('G');
+                    break;
+                case 'T':
+                    rnaStrand.append('A');
+                    break;
+                case 'A':
+                    rnaStrand.append('U');
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid input");
+            }
+        }
+        return rnaStrand.toString();
     }
-
 }
